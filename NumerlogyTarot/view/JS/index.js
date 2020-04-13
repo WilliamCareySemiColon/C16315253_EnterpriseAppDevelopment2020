5700e6428718b492b8c8719843a0471f40f43e76
WilliamCareySemiColon <wc16315253@gmail.com>
2020-04-13 16:24:27 +0100

    added the files to connect the behaviour of the register and login page

--- NumerlogyTarot/view/JS/index.js
+++ NumerlogyTarot/view/JS/index.js
@@ -2,6 +2,18 @@ $("#login").click(function() {
   alert("Hello world");
 });
 
-function ClickMethod() {
-  alert("Hello world");
-}
+//The login button fields
+$("#RegisterInstead").click(function() {
+  $("#Register").show();
+  $("#Login").hide();
+});
+
+////register fields instead
+$("#LoginInstead").click(function() {
+  $("#Login").show();
+  $("#Register").hide();
+});
+
+// $("#Register")
+
+// $("#Login")
