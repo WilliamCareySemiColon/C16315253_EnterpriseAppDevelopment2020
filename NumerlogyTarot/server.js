5a5867223cbe73d712741c365fba8567ff2f801a
WilliamCareySemiColon <wc16315253@gmail.com>
2020-04-15 21:59:43 +0100

    added the connections to the login and register buttons themselves

--- NumerlogyTarot/server.js
+++ NumerlogyTarot/server.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname + "/view")));
 
 app.use(express.urlencoded({ extended: false }));
 // Create our Express-powered HTTP server and have it listen on port 7777
-http.createServer(app).listen(7777, function() {
+http.createServer(app).listen(7777, function () {
   console.log("Listening on port 7777");
 });
 
@@ -28,24 +28,24 @@ var scehma = new mongoose.Schema({
   middlename: "String",
   surname: "String",
   DOB: "String",
-  password: "String"
+  password: "String",
 });
 //creating the connection to the collection itself
 var users = mongoose.model("users", scehma);
 
 // set up our routes
-app.get("/hello", function(req, res) {
+app.get("/hello", function (req, res) {
   // no need to set up HTTP headers
   res.send("Hello World 1234!");
 });
 // simply using res.send instead of res.write and res.end
-app.get("/goodbye", function(req, res) {
+app.get("/goodbye", function (req, res) {
   res.send("Goodbye World!");
 });
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   let filename = "./index.html";
-  fs.readFile(filename, function(err, data) {
+  fs.readFile(filename, function (err, data) {
     if (err) {
       res.writeHead(404, { "Content-Type": "text/html" });
       return res.end("404 Not Found");
@@ -58,15 +58,15 @@ app.get("/", function(req, res) {
 });
 
 //the login method to get the user details with the mongodb server
-app.post("/login", function(req, res) {
+app.post("/checkuserdetails", function (req, res) {
   console.log("body: " + req.body.username + " " + req.body.password);
 
-  users.find({ username: req.body.username }, function(err, docs) {
+  users.find({ username: req.body.username }, function (err, docs) {
     if (err) {
       console.log(error);
     } else {
       HttpMsgs.sendJSON(req, res, {
-        items: docs
+        items: docs,
       });
     }
   });
--- NumerlogyTarot/view/JS/index.js
+++ NumerlogyTarot/view/JS/index.js
@@ -1,52 +1,100 @@
 //The login button fields
-$("#RegisterInsteadBtn").click(function() {
+$("#RegisterInsteadBtn").click(function () {
   ClearLoginDetails();
   $("#Register").show();
   $("#Login").hide();
 });
 
-$("#LoginClearBtn").click(function() {
+$("#LoginClearBtn").click(function () {
   ClearLoginDetails();
 });
 
-$("#LoginBtn").click(function() {
+$("#LoginBtn").click(function () {
   //variables to pass to the server
   var username = document.getElementById("loguname").value;
   var password = document.getElementById("logpass").value;
 
   var data = {
     username: username,
-    password: password
+    password: password,
   };
 
   $.ajax({
     type: "POST",
     data: data,
     dataType: "json",
-    url: "/login",
-    success: function(data) {
-      console.log("success");
-      console.log(JSON.stringify(data));
+    url: "/checkuserdetails",
+    success: function (data) {
+      console.log(data.items[0]);
+      //if the user does not exist
+      if (data.items[0] === undefined) {
+        alert("User with username does not exist");
+      } //if the user exist
+      else {
+        var usernameparm = data.items[0].username;
+        var passwordparm = data.items[0].password;
+        if (username === usernameparm && password === passwordparm) {
+          alert("crediatals are correct");
+          //implement login procedure later
+        } else if (username === usernameparm && password !== passwordparm) {
+          alert("password entered is incorect for the user " + usernameparm);
+        }
+      }
     },
-    error: function(data) {
+    error: function (data) {
+      console.log("error\n\n");
       console.log(data);
-    }
-  });
+    },
+  }); //end ajax
 });
 
 //register button fields instead
-$("#LoginInsteadBtn").click(function() {
+$("#LoginInsteadBtn").click(function () {
   ClearRegisterDetails();
   $("#Login").show();
   $("#Register").hide();
 });
 
-$("#RegisterClearBtn").click(function() {
+$("#RegisterClearBtn").click(function () {
   ClearRegisterDetails();
 });
 
-$("#RegisterBtn").click(function() {
-  alert("test method for now");
+$("#RegisterBtn").click(function () {
+  var fname = document.getElementById("fname").value;
+  var mname = document.getElementById("mname").value;
+  var lname = document.getElementById("lname").value;
+  var reguname = document.getElementById("reguname").value;
+  var email = document.getElementById("email").value;
+  var DOB = document.getElementById("DOB").value;
+  var regpass = document.getElementById("regpass").value;
+  var conpass = document.getElementById("conpass").value;
+  //alert("test method for now");
+  var data = {
+    username: reguname,
+    password: regpass,
+  };
+
+  $.ajax({
+    type: "POST",
+    data: data,
+    dataType: "json",
+    url: "/checkuserdetails",
+    success: function (data) {
+      console.log(data.items[0]);
+      //if the user does not exist
+      if (data.items[0] === undefined) {
+        alert("User with username does not exist");
+        //implement the register details later
+      } //if the user exist
+      else {
+        alert("user with that username - please select a different username");
+      }
+    },
+    error: function (data) {
+      console.log("error\n\n");
+      console.log(data);
+    },
+  }); //end ajax
 });
 
 //methods for clearing the fieids
