7a46cc51c1cec62e27d2588b2f5537b421667f8c
WilliamCareySemiColon <wc16315253@gmail.com>
2020-04-18 22:11:11 +0100

    added the lifepath and birthday calculation method

--- NumerlogyTarot/view/JS/home.js
+++ NumerlogyTarot/view/JS/home.js
@@ -44,6 +44,42 @@ $(document).ready(function () {
   }); //end ajax call
 });
 
+/*The button handlers for the home page of the application itself*/
+$("#LifepathBtn").click(function () {
+  var stringDateObj = Storeddata.DOB.split("/");
+
+  var numberArray = GetLifePathNumber(stringDateObj);
+
+  var elementToDisplay = 0;
+
+  for (var i = 0; i < numberArray.length; i++) {
+    elementToDisplay += numberArray[i];
+  }
+
+  elementToDisplay = GetLifePathNumberFromDays(String(elementToDisplay));
+
+  alert(elementToDisplay);
+});
+
+$("#ExpressionNumberBtn").click(function () {
+  alert("Hello world 2");
+});
+
+$("#SoulUrgeBtn").click(function () {
+  alert("Hello world 3");
+});
+
+$("#PersonalityNumberBtn").click(function () {
+  alert("Hello world 4");
+});
+
+$("#BirthdayNumberBtn").click(function () {
+  var stringDateObj = Storeddata.DOB.split("/");
+
+  elementToDisplay = GetLifePathNumberFromDays(stringDateObj[1]);
+
+  alert(elementToDisplay);
+});
 //Pythagorean Alphabet
 var PythagAlpSinglePoint = ["a", "j", "s"];
 var PythagAlpDoublePoint = ["b", "k", "t"];
@@ -61,3 +97,40 @@ function PythagoreanAlphabetCalculation(
   AllowConstants,
   AllowVowels
 ) {}
+
+//the life path number
+function GetLifePathNumber(StringNumber) {
+  var element = GetLifePathNumberFromDays(StringNumber[2]);
+
+  return [
+    GetLifePathNumberFromDays(StringNumber[0]),
+    GetLifePathNumberFromDays(StringNumber[1]),
+    element,
+  ];
+}
+
+//the lifepath and the birthday number
+function GetLifePathNumberFromDays(StringNumbersDays) {
+  var regularNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 22, 33, 44, 55];
+
+  if (regularNumbers.includes(Number(StringNumbersDays))) {
+    return Number(StringNumbersDays);
+  }
+
+  var sumElement = 0;
+  var LoopedElement = StringNumbersDays;
+
+  do {
+    var elementsArray = [];
+    for (var i = 0; i < LoopedElement.length; i++) {
+      elementsArray.push(Number(LoopedElement[i]));
+      sumElement += elementsArray[i];
+    }
+    if (!regularNumbers.includes(sumElement)) {
+      LoopedElement = String(sumElement);
+      sumElement = 0;
+    }
+  } while (!regularNumbers.includes(sumElement));
+
+  return sumElement;
+}
